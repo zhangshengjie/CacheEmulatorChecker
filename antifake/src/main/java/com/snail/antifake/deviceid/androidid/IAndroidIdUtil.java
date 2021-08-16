efbc19408f22680ee9a4995b976b8b4b753cf240
zhangshengjie <z.cejay@gmail.com>
2021-08-16 15:10:07 +0800

    test

--- antifake/src/main/java/com/snail/antifake/deviceid/androidid/IAndroidIdUtil.java
+++ antifake/src/main/java/com/snail/antifake/deviceid/androidid/IAndroidIdUtil.java
@@ -3,6 +3,7 @@ package com.snail.antifake.deviceid.androidid;
 import android.content.Context;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.util.Log;
 
 /**
  * androidid获取
@@ -14,9 +15,12 @@ public class IAndroidIdUtil {
         String androidId;
         if (!TextUtils.isEmpty(androidId = ISettingUtils.getAndroidPropertyLevel1(context, Settings.Secure.ANDROID_ID))
                 || !TextUtils.isEmpty(androidId = ISettingUtils.getAndroidProperty(context, Settings.Secure.ANDROID_ID))) {
+            Log.e("debug-18","androidId -> "+androidId);
             return androidId;
         }
-        return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        androidId =  Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        Log.e("debug-22","androidId -> "+androidId);
+        return androidId;
     }
 
 }
--- app/src/main/java/com/snail/device/MainActivity.java
+++ app/src/main/java/com/snail/device/MainActivity.java
@@ -42,7 +42,7 @@ import java.util.Map;
 import static com.snail.device.CrashHandlerApplication.getApplication;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private Activity mActivity;
 
@@ -51,6 +51,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mActivity = this;
+
+        findViewById(R.id.btn_dna).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TextView textView = (TextView) findViewById(R.id.tv_getdeviceid);
+                // 不同的版本不一样，4.3之前ITelephony没有getDeviceId
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_PHONE_STATE},
+                            0);
+                    return;
+                }
+                Log.e("debug-66",
+                        "\n 最终方法获取IMEI  \n" + DeviceIdUtil.getDeviceId(mActivity)
+                                + "\n最终方法获取MAC地址 \n" + MacAddressUtils.getMacAddress(mActivity)
+                                + "\n最终方法获取AndroidID \n" + IAndroidIdUtil.getAndroidId(mActivity)
+                                + "\n 是否模拟器  " + EmuCheckUtil.mayOnEmulator(mActivity)
+                                + " \n\n可Hook系统API获取Deviceid\n" + ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId()
+                                + "\n 真实 反Hook Proxy代理获取Deviceid \n" + IPhoneSubInfoUtil.getDeviceIdLevel0(mActivity)
+                                + "\n真实 反Hook Proxy代理获取Deviceid level1\n" + IPhoneSubInfoUtil.getDeviceIdLevel1(mActivity)
+                                + "\n真实 反Hook Proxy代理获取Deviceid level2\n" + IPhoneSubInfoUtil.getDeviceIdLevel2(mActivity)
+                                + "\n 真实 ITelephonyUtil反Hook 获取DeviceId\n" + ITelephonyUtil.getDeviceIdLevel0(mActivity)
+                                + "\n 真实 ITelephonyUtil反Hook 获取DeviceId level1 \n" + ITelephonyUtil.getDeviceIdLevel1(mActivity)
+                                + "\n 自定义ServiceManager获取getDeviceId level2 \n" + ITelephonyUtil.getDeviceIdLevel2(mActivity)
+                                + "\n " + EmuCheckUtil.getCpuInfo()
+                                + "\n " + PropertiesGet.getString("ro.product.cpu.abi")
+                                + "\n 获取链接的路由器地址" + MacAddressUtils.getConnectedWifiMacAddress(getApplication())
+                );
+
+
+                Log.e("debug-84", "系统API反射获取序列号\n" + SysAPIUtil.getSerialNumber(mActivity)
+                        + "\n系统API反射获取序列号\n" + SysAPIUtil.getJavaSerialNumber(mActivity)
+                        + "\n 直接通过 Build Serial " + Build.SERIAL
+                        + "\n 通过ADB Build Serial " + AndroidDeviceIMEIUtil.getSerialno()
+                        + "\n 直接native获取  Serial " + PropertiesGet.getString("ro.serialno")
+                        + "\n 通过系统API获取MAC地址 \n" + SysAPIUtil.getMacAddress(mActivity)
+                        + "\n Iwifmanager 获取mac level 0 \n" + IWifiManagerUtil.getMacAddress(mActivity)
+                        + "\n 通过NetworkInterface获取MAC地址 \n" + MacAddressUtils.getMacAddressByWlan0(mActivity)
+                        + "\n 系统API获取手机型号 （作假） \n" + SysAPIUtil.getPhoneManufacturer()
+                        //Settings.Secure.ANDROID_ID Java类可以被HOOK 并且很简单
+                        + "\n\n通过系统API获取ANDROID_ID (XPOSED可以HOOK) \n " + SysAPIUtil.getAndroidId(mActivity)
+                        + "\n 反射获取系统 ANDROID_IDISettingUtils \n" + ISettingUtils.getAndroidProperty(mActivity, Settings.Secure.ANDROID_ID)
+                        + "\n 反射获取系统 ANDROID_ID ISettingUtils level2 \n" + ISettingUtils.getAndroidPropertyLevel1(mActivity, Settings.Secure.ANDROID_ID)
+                        + "\n native ro.product.manufacturer" + PropertiesGet.getString("ro.product.manufacturer")
+                        + "\n native ro.product.model  " + PropertiesGet.getString("ro.product.model")
+                        + "\n native ro.product.device " + PropertiesGet.getString("ro.product.device")
+                        + "\n native ro.kernel.qemu " + PropertiesGet.getString("ro.kernel.qemu")
+                        + "\n native ro.product.name" + PropertiesGet.getString("ro.product.name")
+
+
+                );
+                if (false) {
+                    AndroidDeviceIMEIUtil.getMac(new IpScanner.OnScanListener() {
+                        @Override
+                        public void scan(Map<String, String> resultMap) {
+                            Log.e("debug-109", resultMap.toString());
+                        }
+                    });
+                }
+
+
+            }
+        });
 
         findViewById(R.id.btn_moni).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,71 +157,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     };
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        if (grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            onClick(null);
-        }
-    }
 
-
-    @Override
-    public void onClick(View v) {
-        TextView textView = (TextView) findViewById(R.id.tv_getdeviceid);
-        // 不同的版本不一样，4.3之前ITelephony没有getDeviceId
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},
-                    0);
-            return;
-        }
-        textView.setText(
-                "\n 最终方法获取IMEI  \n" + DeviceIdUtil.getDeviceId(mActivity)
-                        + "\n最终方法获取MAC地址 \n" + MacAddressUtils.getMacAddress(mActivity)
-                        + "\n最终方法获取AndroidID \n" + IAndroidIdUtil.getAndroidId(mActivity)
-                        + "\n 是否模拟器  " + EmuCheckUtil.mayOnEmulator(mActivity)
-                        + " \n\n可Hook系统API获取Deviceid\n" + ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId()
-                        + "\n 真实 反Hook Proxy代理获取Deviceid \n" + IPhoneSubInfoUtil.getDeviceIdLevel0(mActivity)
-                        + "\n真实 反Hook Proxy代理获取Deviceid level1\n" + IPhoneSubInfoUtil.getDeviceIdLevel1(mActivity)
-                        + "\n真实 反Hook Proxy代理获取Deviceid level2\n" + IPhoneSubInfoUtil.getDeviceIdLevel2(mActivity)
-                        + "\n 真实 ITelephonyUtil反Hook 获取DeviceId\n" + ITelephonyUtil.getDeviceIdLevel0(mActivity)
-                        + "\n 真实 ITelephonyUtil反Hook 获取DeviceId level1 \n" + ITelephonyUtil.getDeviceIdLevel1(mActivity)
-                        + "\n 自定义ServiceManager获取getDeviceId level2 \n" + ITelephonyUtil.getDeviceIdLevel2(mActivity)
-                        + "\n " + EmuCheckUtil.getCpuInfo()
-                        + "\n " + PropertiesGet.getString("ro.product.cpu.abi")
-                        + "\n 获取链接的路由器地址" + MacAddressUtils.getConnectedWifiMacAddress(getApplication())
-        );
-        textView = (TextView) findViewById(R.id.tv_all);
-
-        textView.setText("\n系统API反射获取序列号\n" + SysAPIUtil.getSerialNumber(mActivity)
-                + "\n系统API反射获取序列号\n" + SysAPIUtil.getJavaSerialNumber(mActivity)
-                + "\n 直接通过 Build Serial " + Build.SERIAL
-                + "\n 通过ADB Build Serial " + AndroidDeviceIMEIUtil.getSerialno()
-                + "\n 直接native获取  Serial " + PropertiesGet.getString("ro.serialno")
-                + "\n 通过系统API获取MAC地址 \n" + SysAPIUtil.getMacAddress(mActivity)
-                + "\n Iwifmanager 获取mac level 0 \n" + IWifiManagerUtil.getMacAddress(mActivity)
-                + "\n 通过NetworkInterface获取MAC地址 \n" + MacAddressUtils.getMacAddressByWlan0(mActivity)
-                + "\n 系统API获取手机型号 （作假） \n" + SysAPIUtil.getPhoneManufacturer()
-                //Settings.Secure.ANDROID_ID Java类可以被HOOK 并且很简单
-                + "\n\n通过系统API获取ANDROID_ID (XPOSED可以HOOK) \n " + SysAPIUtil.getAndroidId(mActivity)
-                + "\n 反射获取系统 ANDROID_IDISettingUtils \n" + ISettingUtils.getAndroidProperty(mActivity, Settings.Secure.ANDROID_ID)
-                + "\n 反射获取系统 ANDROID_ID ISettingUtils level2 \n" + ISettingUtils.getAndroidPropertyLevel1(mActivity, Settings.Secure.ANDROID_ID)
-                + "\n native ro.product.manufacturer" + PropertiesGet.getString("ro.product.manufacturer")
-                + "\n native ro.product.model  " + PropertiesGet.getString("ro.product.model")
-                + "\n native ro.product.device " + PropertiesGet.getString("ro.product.device")
-                + "\n native ro.kernel.qemu " + PropertiesGet.getString("ro.kernel.qemu")
-                + "\n native ro.product.name" + PropertiesGet.getString("ro.product.name")
-
-
-        );
-
-        AndroidDeviceIMEIUtil.getMac(new IpScanner.OnScanListener() {
-            @Override
-            public void scan(Map<String, String> resultMap) {
-                Log.v("lishang", resultMap.toString());
-            }
-        });
-    }
 }
